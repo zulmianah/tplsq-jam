e14ff4f8a055684ed457695672ea24d84249ec32
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2020-09-13 12:37:26 +0300

    ch 1 puzzle 1 0.1

--- tplsq/Assets/Scripts/Ch1Puzzle1.cs
+++ tplsq/Assets/Scripts/Ch1Puzzle1.cs
@@ -1,28 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ch1Puzzle1 : MonoBehaviour
 {
     [SerializeField]
-    private Transform[] imageMap;
+    private Transform[] imageMap = null;
 
-    public static bool Success;
+    public static bool Success = false;
+
+    public Ch1Puzzle1()
+    {
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    private void OnMouseDown()
-    {
-        if (!Success)
-            transform.Rotate(0f,0f,90f);
+        Single[] deg = { 0f, 90f, 270f };
+        var ran = new System.Random();
+        int id;
+        foreach (var image in imageMap)
+        {
+            id = 0;
+            //id = ran.Next(3);
+            image.Rotate(0f, 0f, deg[id]);
+        }
+        imageMap[0].Rotate(0f, 0f, deg[2]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (
+            imageMap[0].rotation.z == 0 &&
+            imageMap[1].rotation.z == 0 &&
+            imageMap[2].rotation.z == 0 &&
+            imageMap[3].rotation.z == 0 &&
+            imageMap[4].rotation.z == 0 &&
+            imageMap[5].rotation.z == 0 &&
+            imageMap[6].rotation.z == 0 &&
+            imageMap[7].rotation.z == 0 &&
+            imageMap[8].rotation.z == 0
+            )
+        {
+            Success = true;
+        }
     }
 }
---
+++ tplsq/Assets/Scripts/Ch1Puzzle1Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch1Puzzle1Manager : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        if (!Ch1Puzzle1.Success)
+            transform.Rotate(0f, 0f, 90f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
